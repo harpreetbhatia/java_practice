e790a79b553a69ac8e2e7ee9c4be53d9c009499d
Harpreet <hsbusc@gmail.com>
2016-02-17 01:10:24 +0000

    initial commit

---
+++ codingbat_ex_files/warmup_1/warmup1_ex1.java
@@ -0,0 +1,30 @@
+public class warmup1_ex1{
+    
+    //The parameter weekday is true if it is a weekday, 
+    //and the parameter vacation is true if we are on 
+    //vacation. We sleep in if it is not a weekday or 
+    //we're on vacation. Return true if we sleep in. 
+    
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+      if ((weekday==false)||(vacation==true)){
+        return true;
+      } else {
+        return false;
+      }
+      
+    }
+    
+    public static void main(String []args){
+        if (
+            (sleepIn(false, false)==true) &&
+            (sleepIn(true, false) ==false)&&
+            (sleepIn(false, true) ==true) &&
+            (sleepIn(true, true)  ==true)) 
+        {
+          System.out.println("test passed");
+        } else {
+          System.out.println("test failed");
+        }
+    }
+
+}
