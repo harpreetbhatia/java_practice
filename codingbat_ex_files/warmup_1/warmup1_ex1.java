d74a8a96eac9268efc0e3a72630c37db1a626de0
Harpreet <hsbusc@gmail.com>
2016-02-17 19:28:45 +0000

    edited codingbat warmup1_ex1

--- codingbat_ex_files/warmup_1/warmup1_ex1.java
+++ codingbat_ex_files/warmup_1/warmup1_ex1.java
@@ -1,9 +1,13 @@
-public class warmup1_ex1{
-    
-    //The parameter weekday is true if it is a weekday, 
-    //and the parameter vacation is true if we are on 
-    //vacation. We sleep in if it is not a weekday or 
-    //we're on vacation. Return true if we sleep in. 
+//The parameter weekday is true if it is a weekday, 
+//and the parameter vacation is true if we are on 
+//vacation. We sleep in if it is not a weekday or 
+//we're on vacation. Return true if we sleep in.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class warmup1_ex1{
     
     public static boolean sleepIn(boolean weekday, boolean vacation) {
       if ((weekday==false)||(vacation==true)){
@@ -14,8 +18,8 @@ public class warmup1_ex1{
       
     }
     
-    public static void main(String []args){
-        if (
+    public static void main(String[] args) throws java.lang.Exception {
+    	if (
             (sleepIn(false, false)==true) &&
             (sleepIn(true, false) ==false)&&
             (sleepIn(false, true) ==true) &&
